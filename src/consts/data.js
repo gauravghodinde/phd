a0139440f66a3c4df4fd649cbbf9e87f1b8bf1dc
gauravghodinde <gauravghodinde@gmail.com>
2024-04-27 13:53:55 +0530

    first upload

---
+++ src/consts/data.js
@@ -0,0 +1,22 @@
+
+export const image = "https://lh4.googleusercontent.com/MTb9hkx1FSSxx54aYfQwv8buOBsJZaqu6SJv-Mu-pM6XkAiUnJIiB2Shr5rbPRS9sCYgD006v0__MZCOVijGuPQnE791icglT6sbTXGUmJao47PCe1-o2P2IX2iq7e7jlA=w1280"
+
+
+export const navName = "Soumendu Chakraborty"
+
+export const pages = ["Home","Education","Experience","Publication","Courses","Award & Recognition","students","Announcements"]
+
+
+
+export const homeData = {
+    name : "Dr. Soumendu Chakraborty",
+    post : "Assistant Professor",
+    department: "Department of Computer Science",
+    college: "Indian Institute of Information Technology Lucknow, India",
+    bio: " Soumendu Chakraborty received his B.E. degree in Information Technology from the University Institute of Technology, University of Burdwan, Barddhaman, West Bengal, INDIA. He completed his M.Tech. in Computer Science and Engineering from GLA University, Mathura, India. He has received his Ph.D. in Information Technology from the Indian Institute of Information Technology, Allahabad, U.P., INDIA. He has 15 years of teaching and research experience. He is currently working as an Assistant Professor at the Indian Institute of Information Technology, Lucknow, U.P., INDIA.  He has more than 20 research publications in journals and conferences of repute. His research interests include Computer Vision, Machine Learning, Deep Learning, Image Processing, Biometric Systems, Image Stegnography and Pattern Recognition.",
+    orc: "orcid.org/0000-0002-8778-8229",
+    scorpus: "55627677000",
+    ResearcherID : "ABA-2031-2020",
+    ResearchGateLink: "https://www.researchgate.net/profile/Soumendu-Chakraborty",
+    googleScholar: "https://scholar.google.co.in/citations?user=PGLT85IAAAAJ&hl=en"
+}
---
+++ src/pages/education.js
@@ -0,0 +1,10 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const education = () => {
+  return (
+    <div><Navbar/></div>
+  )
+}
+
+export default education
